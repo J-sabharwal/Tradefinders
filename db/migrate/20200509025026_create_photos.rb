9d791050abd3d506cc89ca230bbd40f6b0f4c109
kzcheng <kevinzifancheng@gmail.com>
2020-05-08 21:35:24 -0700

    Change method of writing

--- db/migrate/20200509025026_create_photos.rb
+++ db/migrate/20200509025026_create_photos.rb
@@ -1,7 +1,7 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-      t.references :review, index: true, foreign_key: true
+      t.belongs_to :review, index: true, foreign_key: true
       t.string :photo_url
 
       t.timestamps null: false
