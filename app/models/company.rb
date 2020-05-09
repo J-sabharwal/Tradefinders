b5d9600e23d3da1f4fcc9771a46813ef82acba33
kzcheng <kevinzifancheng@gmail.com>
2020-05-08 21:35:56 -0700

    Add connection between user and company

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,4 @@
 class Company < ApplicationRecord
   has_many :reviews
+  has_many :users, through: :reviews
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
   has_many :reviews
+  has_many :companies, through: :reviews
 end
