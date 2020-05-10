cb0a4d25432714ee7be0bf43a5d1e4c49a98828b
kzcheng <kevinzifancheng@gmail.com>
2020-05-10 12:45:56 -0700

    Implement basic 1-10 choice

--- client/src/js/Review/ReviewForm.js
+++ client/src/js/Review/ReviewForm.js
@@ -1,58 +1,125 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import '../../styles/App.css';
+// import React, { Component } from 'react';
+// import axios from 'axios';
+// import 'typeface-roboto';
+// import { makeStyles } from '@material-ui/core/styles';
+// import TextField from '@material-ui/core/TextField';
 
-class ReviewForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      company: {},
-      review: {}
-    };
-  }
+// class ReviewForm extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       company: {},
+//       review: {}
+//     };
+//   }
   
   
-  async componentDidMount() {
-    const {match: {params}} = this.props;
-    console.log(params);
+//   async componentDidMount() {
+//     const {match: {params}} = this.props;
+//     console.log(params);
 
          
-    // Promise.all([companyDeets, companyReviews])
-    //   .then((all) => {
-    //   console.log(all.data)
-    //   this.setState(prev => ({
-    //     ...prev,
-    //     company: all.data.company,
-    //     review: all.data.review
-    //   }));
+//     // Promise.all([companyDeets, companyReviews])
+//     //   .then((all) => {
+//     //   console.log(all.data)
+//     //   this.setState(prev => ({
+//     //     ...prev,
+//     //     company: all.data.company,
+//     //     review: all.data.review
+//     //   }));
 
-    // axios.get(`/api/company/${params.id}`)
-    // .then((response) => {
+//     // axios.get(`/api/company/${params.id}`)
+//     // .then((response) => {
       
-    //   console.log(response.data)
-    //   this.setState({
-    //     company: response.data.company
-    //   });
-    // })
-  }
-
-  render() {
-    return (
-      <>
-        <article className="profile">
-          <header className="company-name">{this.state.company.name}</header>
-          <ul className="company-info">
-            <li>{this.state.company.email}</li>
-            <li>{this.state.company.phone_number}</li>
-            <li>{this.state.company.description}</li>
-          </ul>
-          <h5>{this.state.review.comment}</h5>
-        </article>
-      </>
-    );
-  }
+//     //   console.log(response.data)
+//     //   this.setState({
+//     //     company: response.data.company
+//     //   });
+//     // })
 
 
+//   }
+
+//   render() {
+//     return (
+//       <article className="profile">
+//         <form className="ReviewForm" noValidate>
+//           <div>
+//             <TextField
+//               id="outlined-select-currency"
+//               select
+//               label="Punctuality"
+//               value={currency}
+//               onChange={handleChange}
+//               helperText="Please select your currency"
+//               variant="outlined"
+//             >
+//               {currencies.map((option) => (
+//                 <MenuItem key={option.value} value={option.value}>
+//                   {option.label}
+//                 </MenuItem>
+//               ))}
+//             </TextField>
+//           </div>
+//         </form>
+//       </article>
+//     );
+//   }
+
+
+// }
+
+// export default ReviewForm;
+
+
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
+const a = Array.from(Array(11).keys());
+
+export default function MultilineTextFields() {
+  const classes = useStyles();
+  const [scorePunctuality, setScorePunctuality] = React.useState(0);
+  // const [scoreCleanliness, setScoreCleanliness] = React.useState(0);
+  // const [scoreCommunication, setScoreCommunication] = React.useState(0);
+  // const [scorePrice, setScorePrice] = React.useState(0);
+
+  const handleChange = (event) => {
+    setScorePunctuality(event.target.value);
+  };
+
+  console.log(a);
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField
+          id="outlined-select-currency"
+          select
+          label="Select"
+          value={scorePunctuality}
+          onChange={handleChange}
+          helperText="Please select your currency"
+          variant="outlined"
+        >
+          {a.map((number) => (
+            <MenuItem key={number} value={number}>
+              {number}
+            </MenuItem>
+          ))}
+        </TextField>
+      </div>
+    </form>
+  );
 }
-
-export default ReviewForm;
