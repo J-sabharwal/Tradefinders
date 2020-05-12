3f986b307f275c9ad06e31dab5c7a32c318cb4b6
kzcheng <kevinzifancheng@gmail.com>
2020-05-11 17:46:37 -0700

    Change implementation of search requirments to allow empty params to be ignored

--- app/controllers/api/company_controller.rb
+++ app/controllers/api/company_controller.rb
@@ -2,10 +2,11 @@ class Api::CompanyController < ApplicationController
   def index
     @companies = Company.all
 
-    #TODO Find an easier way to implement this.
-    if params[:trade_type]
+    unless params[:trade_type].to_s.strip.empty?
       @companies = @companies.where(trade_type: params[:trade_type].downcase)
-    elsif params[:location]
+    end
+
+    unless params[:location].to_s.strip.empty?
       @companies = @companies.where(location: params[:location])
     end
 
