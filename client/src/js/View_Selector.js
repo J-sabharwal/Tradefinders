686cbb95da4dcfc92aa0af0467ee91e1cc9632e0
Brad Ruud <bradruud@gmail.com>
2020-05-12 04:38:37 +0000

    add results page

--- client/src/js/View_Selector.js
+++ client/src/js/View_Selector.js
@@ -4,31 +4,40 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import CompanyDetails from "./Company/CompanyDetails";
 import ReviewForm from './Review/ReviewForm';
 import SearchBar from './SearchBar';
+import SearchResults from './Results/SearchResults'
 
 
 export default function ViewSelector() {
 
   return (
     <>
-      
       <Router>
         <div className="company-details">
-          <Route exact path="/company/:id" component= {CompanyDetails} />
+          <Route exact path="/company/:id"
+            component={CompanyDetails} />
         </div>
       </Router>
 
       <Router>
         <div className="review-form">
-          <Route exact path='/review/:company_id/new'   component={ReviewForm} />
+          <Route exact path='/review/:company_id/new'
+            component={ReviewForm} />
         </div>
       </Router>
 
       <Router>
         <section className="background">
-          <Route exact path="/" component={SearchBar} />
+          <Route exact path="/"
+            component={SearchBar} />
         </section>
       </Router>
-      
+
+      <Router>
+        <section className="search-results">
+          <Route exact path="/results"
+            component={SearchResults} />
+        </section>
+      </Router>
     </>
   );
 }
