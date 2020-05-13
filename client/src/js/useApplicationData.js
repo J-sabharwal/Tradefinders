01027975e51a03bc69f66ee92cb9739b9d59bc57
kzcheng <kevinzifancheng@gmail.com>
2020-05-12 19:09:05 -0700

    Revert "Fix search issue thing"
    This reverts commit 3d5d04dee6fd431a0667de0999ec22ec27d9510a.

--- client/src/js/useApplicationData.js
+++ client/src/js/useApplicationData.js
@@ -1,72 +1,52 @@
-import { useEffect, useState } from "react";
+import React from 'react';
 import axios from 'axios';
 
 export default function useApplicationData(props) {
-  const [searchParams, setSearchParams] = useState({
+  const [state, setState] = React.useState({
     trade: "",
     location: "",
+    companies: [],
   });
-  const [companyList, setCompanyList] = useState([]);
-
-  useEffect(() => {
-    const companySearch = axios
-      .get(`/api/company?trade_type=${searchParams.trade}&location=${searchParams.location}`);
-    
-    Promise.all([companySearch]).then((res) => {
-      const companies = "companies";
-      setCompanyList(res[0].data.companies);
-    }).catch((error) => {
-      console.log(error);
-    });
-  }, [searchParams]);
 
 
   // handleTradeChange, handleLocationChange, and Search are all functions required by SearchBar.js - Brad
   const handleTradeChange = (event) => {
     const trade = event.target.name;
-    setSearchParams({
-      ...searchParams,
+    setState({
+      ...state,
       [trade]: event.target.value,
     });
   };
 
   const handleLocationChange = (event) => {
     const location = event.target.name;
-    setSearchParams({
-      ...searchParams,
+    setState({
+      ...state,
       [location]: event.target.value,
     });
   };
 
-  // const Search = () => {
-  //   const companySearch = axios
-  //     .get(`/api/company?trade_type=${searchParams.trade}&location=${searchParams.location}`);
-  //   // const companiesByLocation = axios
-  //   //   .get(`/api/company?location=${state.location}`);
+  const Search = () => {
+    const companySearch = axios
+      .get(`/api/company?trade_type=${state.trade}&location=${state.location}`);
+    // const companiesByLocation = axios
+    //   .get(`/api/company?location=${state.location}`);
 
-  //   Promise.all([companySearch])
-  //     .then((response) => {
-  //       const companies = "companies";
-  //       // console.log(response[0].data.companies)
-  //       setSearchParams({
-  //         ...searchParams,
-  //         [companies]: response[0].data.companies,
-  //       });
-  //       // Leaving the below code in, to implement a "no-results" error at a later time. - Brad
-  //       // if (response[0].data.companies.length === 0) {
-  //       // }
-  //     });
-  // };
-  // console.log(searchParams.companies);
+    Promise.all([companySearch])
+      .then((response) => {
+        const companies = "companies";
+          // console.log(response[0].data.companies)
+          setState({
+            ...state,
+            [companies]: response[0].data.companies,
+          });
+        // Leaving the below code in, to implement a "no-results" error at a later time. - Brad
+        // if (response[0].data.companies.length === 0) {
+        // }
+        });
+  }
+  console.log(state.companies);
   
-  const search = () => {
-    console.log(companyList);
-  };
 
-  return {
-    state: {...searchParams, companyList},
-    search,
-    handleLocationChange,
-    handleTradeChange
-  };
-}
+  return { state, Search, handleLocationChange, handleTradeChange };
+};
