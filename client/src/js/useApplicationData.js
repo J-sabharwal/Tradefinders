e392f3aaacb2eca70bac3f6cb7fbfbde88d77f01
Brad Ruud <bradruud@gmail.com>
2020-05-14 19:41:00 +0000

    add fix for search result message with empty input

--- client/src/js/useApplicationData.js
+++ client/src/js/useApplicationData.js
@@ -14,38 +14,36 @@ export default function useApplicationData(props) {
   // handleTradeChange, handleLocationChange, and Search are all functions required by SearchBar.js - Brad
   const handleTradeChange = (event) => {
     const trade = event.target.name;
-    setState({
-      ...state,
-      [trade]: event.target.value,
-    });
+    if (event.target.value !== "") {
+      setState({
+        ...state,
+        [trade]: event.target.value,
+      });
+    }
   };
 
   const handleLocationChange = (event) => {
     const location = event.target.name;
-    setState({
-      ...state,
-      [location]: event.target.value,
-    });
+    if (event.target.value !== "") {
+      setState({
+        ...state,
+        [location]: event.target.value,
+      });
+    }
   };
 
   const Search = () => {
     const companySearch = axios
       .get(`/api/company?trade_type=${state.trade}&location=${state.location}`);
-    // const companiesByLocation = axios
-    //   .get(`/api/company?location=${state.location}`);
 
     Promise.all([companySearch])
       .then((response) => {
         const companies = "companies";
-        // console.log(response[0].data.companies)
         setState({
           ...state,
           [companies]: response[0].data.companies,
           goToSearchResults: true,
         });
-        // Leaving the below code in, to implement a "no-results" error at a later time. - Brad
-        // if (response[0].data.companies.length === 0) {
-        // }
       });
     
   };
