99ec64bc9b5fc5bdf96a0c979a6a07060fce010e
Brad Ruud <bradruud@gmail.com>
2020-05-14 20:32:03 +0000

    add average ratings in search results

--- client/src/js/Results/SearchResults.js
+++ client/src/js/Results/SearchResults.js
@@ -2,14 +2,12 @@ import React from 'react';
 
 import SearchResultItem from './SearchResultItem';
 
-
+//render all cards for search results
 export default function SearchResults(props) {
 
   // Kevin: When you need the companies, just get it like this.
   // Type props.location.state.companies
   const companies = props.companies;
-
-  //Average rating needs to be passed to the return below, to be passed as props to SearchResultItem.js - Brad
 
   return companies.map(company => {
 
@@ -23,7 +21,6 @@ export default function SearchResults(props) {
             trade={company.trade_type}
             location={company.location}
             photo={company.photo_url}
-            // rating={state.review.total_avg}
           />
       </>
     );
