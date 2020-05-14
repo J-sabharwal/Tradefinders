1d8f9e2743df8576a4800467ec1cc97647007d3f
kzcheng <kevinzifancheng@gmail.com>
2020-05-13 20:00:15 -0700

    Navbar can display logged in user now (with bugs)

--- client/src/js/navbar.js
+++ client/src/js/navbar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Cookies from 'universal-cookie';
+import Axios from "axios";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
@@ -24,6 +26,35 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const cookies = new Cookies();
+  const [currentUser, setCurrentUser] = React.useState(undefined);
+
+  const getCurrentUser = () => {
+    let userEmail = cookies.get('userEmail');
+    console.log(userEmail);
+    if (userEmail) {
+      Axios.get(`/api/user?email=${userEmail}`)
+        .then((res) => {
+          console.log(res);
+          console.log(res.data.users[0]);
+          setCurrentUser(res.data.users[0]);
+        });
+    } else {
+      setCurrentUser(undefined);
+    }
+  };
+
+  const LoginButton = () => {
+    getCurrentUser();
+    if (currentUser) {
+      return <>
+        <Button color="inherit">Welcome {currentUser.name}!</Button>
+        <Button color="inherit">Logout</Button>
+      </>;
+    } else {
+      return <Button color="inherit">Login</Button>;
+    }
+  };
 
   return (
     <div className={classes.root}>
@@ -44,7 +75,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          <LoginButton/>
         </Toolbar>
       </AppBar>
     </div>
